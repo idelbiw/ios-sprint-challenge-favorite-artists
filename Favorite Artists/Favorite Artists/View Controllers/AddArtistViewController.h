bc2b178d7583495be1823c513ada70837f9d5939
Waseem Idelbi <idelbiw@gmail.com>
2020-09-25 21:28:58 -0400

    created view controller files and subclassed the view controllers in the storyboard after them

---
+++ Favorite Artists/Favorite Artists/View Controllers/AddArtistViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddArtistViewController.h
+//  Favorite Artists
+//
+//  Created by Waseem Idelbi on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddArtistViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/View Controllers/FavoriteArtistsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  FavoriteArtistsTableViewController.h
+//  Favorite Artists
+//
+//  Created by Waseem Idelbi on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FavoriteArtistsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
