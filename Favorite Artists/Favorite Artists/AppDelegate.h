ba39d21219393a62889b3fed20cced1912d223a3
Waseem Idelbi <idelbiw@gmail.com>
2020-09-25 21:25:28 -0400

    Created project and finished adding all UI elements in the storyboard

---
+++ Favorite Artists/Favorite Artists/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Favorite Artists
+//
+//  Created by Waseem Idelbi on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Favorite Artists
+//
+//  Created by Waseem Idelbi on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
